f881c261f6a6719762c20e0d5af0f37d0bd27ddf
Yashik Rammaryian <Yashik.R@catin.onmicrosoft.com>
2024-07-20 16:00:58 +0200

    Created Orders Table

---
+++ Boxer.DB/dbo/Tables/Orders.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Orders]
+(
+	[OrderID] INT IDENTITY (1, 1) NOT NULL, 
+    [OrderNumber] INT NOT NULL, 
+    [Items] NVARCHAR (50) NOT NULL, 
+    [Quantity] INT NOT NULL, 
+    [Date] DATE NOT NULL, 
+    [Price] FLOAT NOT NULL, 
+    [Supplier] NVARCHAR (50) NOT NULL, 
+    [DeliveryDate] DATE NOT NULL, 
+    CONSTRAINT [PK_Orders] PRIMARY KEY ([OrderID]),
+    CONSTRAINT [UC_Orders] UNIQUE ([OrderNumber])
+)
